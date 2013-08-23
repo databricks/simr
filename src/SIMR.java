816b27e6ccf65592189bbfb03d31374c6d6ec60b
Ali Ghodsi <alig@cs.berkeley.edu>
2013-08-23 15:21:28 -0700

    Beginning

--- src/SIMR.java
+++ src/SIMR.java
@@ -14,6 +14,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -23,6 +24,62 @@ import org.apache.hadoop.util.GenericOptionsParser;
 
 public class SIMR {
 
+	static class RandomInputFormat implements InputFormat<Text, Text> {
+
+		/**
+		 * Generate the requested number of file splits, with the filename
+		 * set to the filename of the output file.
+		 */
+		public InputSplit[] getSplits(JobConf job,
+									  int numSplits_) throws IOException {
+			int numSplits = new JobClient().getClusterStatus().getTaskTrackers();
+			InputSplit[] result = new InputSplit[numSplits];
+			Path outDir = org.apache.hadoop.mapred.FileOutputFormat.getOutputPath(job);
+			for(int i=0; i < result.length; ++i) {
+				result[i] = new FileSplit(new Path(outDir, "dummy-split-" + i), 0, 1,
+						(String[])null);
+			}
+			return result;
+		}
+
+		/**
+		 * Return a single record (filename, "") where the filename is taken from
+		 * the file split.
+		 */
+		static class RandomRecordReader implements RecordReader<Text, Text> {
+			Path name;
+			public RandomRecordReader(Path p) {
+				name = p;
+			}
+			public boolean next(Text key, Text value) {
+				if (name != null) {
+					key.set(name.getName());
+					name = null;
+					return true;
+				}
+				return false;
+			}
+			public Text createKey() {
+				return new Text();
+			}
+			public Text createValue() {
+				return new Text();
+			}
+			public long getPos() {
+				return 0;
+			}
+			public void close() {}
+			public float getProgress() {
+				return 0.0f;
+			}
+		}
+
+		public RecordReader<Text, Text> getRecordReader(InputSplit split,
+														JobConf job,
+														Reporter reporter) throws IOException {
+			return new RandomRecordReader(((FileSplit) split).getPath());
+		}
+	}
 
     public static String getLocalIP() {
 	String ip;
@@ -72,6 +129,7 @@ public class SIMR {
 	    System.exit(2);
 	}
 	Job job = new Job(conf, "SIMR");
+
 	job.setNumReduceTasks(0);
 	job.setJarByClass(SIMR.class);
 	job.setMapperClass(TokenizerMapper.class);
