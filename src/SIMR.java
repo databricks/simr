5027c6d2a51107a3dc192b9de0dd9b0383796ed1
Ali Ghodsi <alig@cs.berkeley.edu>
2013-08-24 16:51:26 -0700

    shifted to new example

--- src/SIMR.java
+++ src/SIMR.java
@@ -11,77 +11,73 @@ import java.util.*;
 import java.net.*;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapred.*;
-import org.apache.hadoop.mapred.lib.IdentityReducer;
-import org.apache.hadoop.mapred.lib.NullOutputFormat;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
 import org.apache.hadoop.fs.*;
-import org.apache.hadoop.hdfs.*;
 
-public class SIMR extends Configured implements Tool {
+public class SIMR {
 
-	static class RandomInputFormat implements InputFormat<Text, Text> {
-
+	static class RandomInputFormat extends InputFormat<Text, Text> {
 		/**
 		 * Generate the requested number of file splits, with the filename
 		 * set to the filename of the output file.
 		 */
-		public InputSplit[] getSplits(JobConf job,
-									  int numSplits) throws IOException {
-			InputSplit[] result = new InputSplit[numSplits];
-			Path outDir = org.apache.hadoop.mapred.FileOutputFormat.getOutputPath(job);
+		public List<InputSplit> getSplits(JobContext context) throws IOException {
+			int clusterSize = Integer.parseInt(context.getConfiguration().get("NumTaskTrackers"));
+			InputSplit[] result = new InputSplit[clusterSize];
+//			Path outDir = org.apache.hadoop.mapred.FileOutputFormat.getOutputPath(job);
 			for(int i=0; i < result.length; ++i) {
-				result[i] = new FileSplit(new Path(outDir, "dummy-split-" + i), 0, 1,
+				result[i] = new FileSplit(new Path("dummy-split-" + i), 0, 1,
 						(String[])null);
 			}
-			return result;
+			return Arrays.asList(result);
 		}
 
 		/**
 		 * Return a single record (filename, "") where the filename is taken from
 		 * the file split.
 		 */
-		static class RandomRecordReader implements RecordReader<Text, Text> {
+		static class RandomRecordReader extends RecordReader<Text, Text> {
 			Path name;
-			public RandomRecordReader(Path p) {
-				name = p;
+			boolean first = true;
+
+			public void initialize(InputSplit split, TaskAttemptContext context) throws java.io.IOException, java.lang.InterruptedException {
+				name = ((FileSplit) split).getPath();
 			}
-			public boolean next(Text key, Text value) {
-				if (name != null) {
-					key.set(name.getName());
-					name = null;
+
+			public boolean nextKeyValue() {
+				if (first) {
+					first = false;
 					return true;
 				}
 				return false;
 			}
-			public Text createKey() {
-				return new Text();
+
+			public Text getCurrentKey() throws java.io.IOException, java.lang.InterruptedException {
+				return new Text(name.getName());
 			}
-			public Text createValue() {
-				return new Text();
+
+			public Text getCurrentValue() throws java.io.IOException, java.lang.InterruptedException {
+				return new Text("");
 			}
-			public long getPos() {
-				return 0;
-			}
-			public void close() {}
-			public float getProgress() {
-				return 0.0f;
-			}
+
+			public void close() { }
+
+			public float getProgress() { return 0.0f; }
 		}
 
-		public RecordReader<Text, Text> getRecordReader(InputSplit split,
-														JobConf job,
-														Reporter reporter) throws IOException {
-			return new RandomRecordReader(((FileSplit) split).getPath());
+		public RecordReader<Text, Text> createRecordReader(InputSplit split, TaskAttemptContext context) throws java.io.IOException, InterruptedException {
+//
+//		public RecordReader<Text, Text> getRecordReader(InputSplit split,
+//														JobConf job,
+//														Reporter reporter) throws IOException {
+			return new RandomRecordReader();
 		}
 	}
 
@@ -108,70 +104,48 @@ public class SIMR extends Configured implements Tool {
 	return null;
    }
 
-	static class MapClient extends MapReduceBase
-			implements Mapper<Object, Text, Text, Text>{
+	public static class MyMapper
+			extends Mapper<Object, Text, Text, IntWritable>{
 
-		@Override
-		public void map(Object key, Text value,
-						OutputCollector<Text, Text> out,
-						Reporter reporter) throws IOException {
-			//To change body of implemented methods use File | Settings | File Templates.
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		public void map(Object key, Text value, Context context
+		) throws IOException, InterruptedException {
+
 			Configuration conf = new Configuration();
 			FileSystem fs = FileSystem.get(conf);
 			FSDataOutputStream outf = fs.create(new Path(getLocalIP()), true);
 			outf.close();
 
-//			out.collect(new Text(getLocalIP()), new Text(""));
 		}
 	}
 
-	public int run(String[] args) throws Exception {
-		if (args.length == 0) {
-			System.out.println("Usage: SIMR <out-dir>");
-			ToolRunner.printGenericCommandUsage(System.out);
-			return -1;
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		if (otherArgs.length != 1) {
+			System.err.println("Usage: SIMR <out>");
+			System.exit(2);
 		}
 
-		Path outDir = new Path(args[0]);
-		JobConf job = new JobConf(getConf());
 
+		int clusterSize = new JobClient().getClusterStatus().getTaskTrackers();
+		System.out.println("Cluster size: " + clusterSize);
+		conf.set("NumTaskTrackers", Integer.toString(clusterSize));
+ 		Job job = new Job(conf, "SIMR3");
+
+		job.setNumReduceTasks(0);
 		job.setJarByClass(SIMR.class);
-		job.setJobName("SIMR2");
-		org.apache.hadoop.mapred.FileOutputFormat.setOutputPath(job, outDir);
+		job.setMapperClass(MyMapper.class);
 
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(Text.class);
-
-		job.setInputFormat(RandomInputFormat.class);
-		job.setMapperClass(MapClient.class);
-//		job.setReducerClass(IdentityReducer.class);
-//		job.setOutputFormat(TextOutputFormat.class);
-		job.setOutputFormat(NullOutputFormat.class);
-
-		JobClient client = new JobClient(job);
-		ClusterStatus cluster = client.getClusterStatus();
-		Integer clusterSize = cluster.getTaskTrackers();
-
-
-		// reducer NONE
-		job.setNumReduceTasks(0);
-
-		Date startTime = new Date();
-		System.out.println("Job started: " + startTime + " with " + clusterSize + " task trackers");
-//		job.set("clustersize", clusterSize.toString());
-		JobClient.runJob(job);
-		Date endTime = new Date();
-		System.out.println("Job ended: " + endTime);
-		System.out.println("The job took " +
-				(endTime.getTime() - startTime.getTime()) /1000 +
-				" seconds.");
-
-		return 0;
-	}
-
-	public static void main(String[] args) throws Exception {
-		int res = ToolRunner.run(new Configuration(), new SIMR(), args);
-		System.exit(res);
+		job.setInputFormatClass(RandomInputFormat.class);
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 
 }
