72ed45f66852258c7cc50dcec8cb9937bdc8916c
Ali Ghodsi <alig@cs.berkeley.edu>
2013-09-26 14:19:40 -0700

    debug

--- src/main/java/org/apache/spark/simr/Simr.java
+++ src/main/java/org/apache/spark/simr/Simr.java
@@ -106,12 +106,6 @@ public class Simr {
 //            System.out.println(url.getFile());
 //        }
 //
-        String sep = System.getProperty("path.separator");
-        String classpath = System.getProperty("java.class.path");
-        System.out.println("java.class.path=" + classpath);
-        System.out.println("CLASSPATH=" + System.getenv("CLASSPATH"));
-        System.out.println("new CLASSPATH=" + System.getenv("CLASSPATH") + sep + classpath);
-        System.getenv().put("CLASSPATH", System.getenv("CLASSPATH") + sep + classpath);
 
         try {
             redirectOutput("driver");
