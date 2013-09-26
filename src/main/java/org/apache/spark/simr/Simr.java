7150a2c4e5251ff7835e13d4579f2119796886d4
Ali Ghodsi <alig@cs.berkeley.edu>
2013-09-26 16:42:39 -0700

    debug

--- src/main/java/org/apache/spark/simr/Simr.java
+++ src/main/java/org/apache/spark/simr/Simr.java
@@ -99,7 +99,7 @@ public class Simr {
 
     public void startShell() {
         try {
-            redirectOutput("driver");
+//            redirectOutput("driver");
             org.apache.spark.repl.SimrReplServer.main(new String[]{
                     conf.get("simr_tmp_dir") + "/" + SHELLURL,
                     getLocalIP() });
