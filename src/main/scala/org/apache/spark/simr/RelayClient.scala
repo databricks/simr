e9442c206e9d1b18e0f6f4184d6cd99ff2b5537b
Ahir Reddy <ahirreddy@gmail.com>
2013-10-24 00:58:26 -0700

    Moved SimrRepl from RelayClient to separate SimrRepl file

--- src/main/scala/org/apache/spark/simr/RelayClient.scala
+++ src/main/scala/org/apache/spark/simr/RelayClient.scala
@@ -1,6 +1,5 @@
 package org.apache.spark.simr
 
-import java.io.PrintWriter
 import java.util.concurrent.TimeoutException
 import java.net.{NetworkInterface, Inet4Address}
 
@@ -17,7 +16,6 @@ import org.apache.hadoop.fs.{Path, FileSystem}
 
 import org.apache.spark.util.AkkaUtils
 import org.apache.spark.Logging
-import org.apache.spark.repl.SparkILoop
 
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
@@ -197,29 +195,6 @@ object RelayClient extends Logging {
       readLoop(client)
       actorSystem.shutdown()
     }
-  }
-}
-
-object SimrRepl extends Logging {
-  def main(args: Array[String]) {
-    val simrUrl = args(0)
-
-    lazy val urls = java.lang.Thread.currentThread.getContextClassLoader match {
-      case cl: java.net.URLClassLoader => cl.getURLs.toList
-      case _ => sys.error("classloader is not a URLClassLoader")
-    }
-
-    val interp = new SparkILoop(Console.in, new PrintWriter(Console.out, true), simrUrl)
-
-    org.apache.spark.repl.Main.interp = interp
-    interp.setPrompt("\n" + RelayClient.SIMR_PROMPT)
-
-    interp.settings = new scala.tools.nsc.Settings
-    val urlStrs = urls.map(_.toString.replaceAll("^file:/","/"))
-
-    interp.addAllClasspath(urlStrs)
-    interp.process(Array[String]())
-    interp.command("sc.stop()")
   }
 }
 
